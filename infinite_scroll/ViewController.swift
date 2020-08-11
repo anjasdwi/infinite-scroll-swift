8f2d31c2d1d9fecc7b1b7865796a988d1e904f34
Anjas Dwi <anjasdwi@bareksa.com>
2020-08-11 17:12:45 +0700

    Initial Commit

---
+++ infinite_scroll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  infinite_scroll
+//
+//  Created by Anjas Dwi on 11/08/20.
+//  Copyright © 2020 Anjas Dwi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
